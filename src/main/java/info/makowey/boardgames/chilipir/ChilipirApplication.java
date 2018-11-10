1a2be27ce00c8395b2d0504a1b74f522e0095b42
Claudiu Macovei <macovei_cef@yahoo.com>
2018-11-10 09:29:54 +0200

    setup exec

--- src/main/java/info/makowey/boardgames/chilipir/ChilipirApplication.java
+++ src/main/java/info/makowey/boardgames/chilipir/ChilipirApplication.java
@@ -1,29 +1,27 @@
 package info.makowey.boardgames.chilipir;
 
-import info.makowey.boardgames.chilipir.repository.BoardGameRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Slf4j
 @SpringBootApplication
-public class ChilipirApplication implements CommandLineRunner {
+public class ChilipirApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
-	private final BoardGameRepository repository;
+    public static void main(String[] args) {
+        SpringApplication.run(ChilipirApplication.class, args);
+    }
 
-	@Autowired
-	public ChilipirApplication( BoardGameRepository repository ) {
-		this.repository = repository;
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChilipirApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChilipirApplication.class, args);
-	}
-
-	@Override
-	public void run( String... args ) {
-		log.info( "!!! UP & RUNNING !!!" );
-	}
+    @Override
+    public void run(String... args) {
+        log.info("!!! SERVLET UP & RUNNING !!!");
+    }
 }
