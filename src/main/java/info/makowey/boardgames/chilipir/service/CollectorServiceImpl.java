be685432671e3f1bca8224050c9bfe099ec985e1
Claudiu Macovei <claudiu.macovei@visma.com>
2020-12-02 17:33:55 +0200

    update min price

--- src/main/java/info/makowey/boardgames/chilipir/service/CollectorServiceImpl.java
+++ src/main/java/info/makowey/boardgames/chilipir/service/CollectorServiceImpl.java
@@ -65,8 +65,8 @@ public class CollectorServiceImpl implements CollectorService {
 	public List<BoardGame> traceAll() {
 		Query query = new Query();
 		//query.skip(pageNumber * pageSize);
-		query.addCriteria( Criteria.where( "currentPrice" ).gte( 100 ) );
-		query.limit( 300 );
+		query.addCriteria( Criteria.where( "currentPrice" ).gte( -1 ) );
+		query.limit( 321 );
 		return mongoTemplate.find( query, BoardGame.class );
 	}
 
